e6971108e37c0a9c24aa762c3b31ff0b51c162c4
Sukera <Seelengrab@users.noreply.github.com>
2018-09-21 15:40:02 +0200

    CHANGED FileLogger doc to mention flushing & appending behaviour

--- src/FileLogger.jl
+++ src/FileLogger.jl
@@ -1,7 +1,7 @@
 """
-A generic file logger for logging to files. Flushes the necessary output stream after each write (i.e. after each logging event) and closes the files on finalizing.
+A generic file logger for logging to files. Flushes the necessary output stream after each write (i.e. after each logging event) by default and closes the files on finalizing. Opened files are by default appended to. Given `append = false`, they will be overwritten.
 
-    FileLogger(logPaths = Dict(Info => "default.log"))
+    FileLogger(logPaths = Dict(Info => "default.log"); flush = true, append = true)
 
 Logs logging events with LogLevel greater than or equal to `Info` to "default.log", should no `logPaths` be given. In case two LogLevels are present, e.g. `Info` and `Error`, all logging events from `Info` up to (but excluding) `Error` will be logged to the file given by `Info`. `Error` and above will be logged to the file given by `Error`. It is possible to "clamp" logging events, by providing an upper bound that's logging to `/dev/null` on Unix/Mac or `NUL` on Windows. Beware, as the message will still be composed before writing to the actual file (no hotwiring).
 
